a83adfebb45f7e445b4b880781a23b0cd538742b
Pavithra M G <pavithramg@Pavithras-MacBook-Air.local>
2025-05-24 18:01:38 -0700

    freshcredit

--- banking-main/app/layout.tsx
+++ banking-main/app/layout.tsx
@@ -12,8 +12,8 @@ const ibmPlexSerif = IBM_Plex_Serif({
 })
 
 export const metadata: Metadata = {
-  title: "Horizon",
-  description: "Horizon is a modern banking platform for everyone.",
+  title: "FreshCredit",
+  description: "FreshCredit.",
   icons: {
     icon: '/icons/logo.svg'
   }
